6a04f421d259bbea3986bf2dec9afacbeee4c17a
jrwallace1983 <jrwallace1983@gmail.com>
2019-09-18 23:23:26 -0700

    fixed home redirect

--- src/containers/main.js
+++ src/containers/main.js
@@ -116,7 +116,7 @@ class Main extends Component{
             <React.Fragment>
                 <Navbar></Navbar>
                 <Switch>
-                <Route path='/home' exact render={(props) =>
+                <Route path='/' exact render={(props) =>
                     <React.Fragment>
                     <Home></Home>
                     </React.Fragment>}/>
